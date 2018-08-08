afd04e37c3a63f500d693dc1c20fdb0a31ebd759
Doogle <fdunigan11@gmail.com>
2018-08-08 17:53:59 -0400

    API Friends List up and running, routes up-to-date

---
+++ app/data/friends.js
@@ -0,0 +1,165 @@
+var friends = [
+{
+    "name": "Bruce",
+    "photo": "https://assets3.thrillist.com/v1/image/1299823/size/tmg-article_default_mobile.jpg",
+    "scores": [
+        4,
+        2,
+        4,
+        1,
+        2,
+        1,
+        5,
+        3,
+        2,
+        4
+    ]
+},
+{
+    "name": "Lilith",
+    "photo": "https://leadershipspirit.files.wordpress.com/2013/10/stock_2013_hi-res-download_2_1.jpg",
+    "scores": [
+        1,
+        5,
+        3,
+        4,
+        4,
+        1,
+        3,
+        2,
+        5,
+        1
+    ]
+},
+{
+    "name": "Steve",
+    "photo": "http://www.creditlenders.info/wp-content/uploads/stock-photo-people-20-free-stock-photos-of-people.jpg",
+    "scores": [
+        3,
+        3,
+        3,
+        1,
+        2,
+        5,
+        5,
+        4,
+        2,
+        4
+    ]
+},
+{
+    "name": "Dan",
+    "photo": "https://www.freegreatpicture.com/files/94/28351-business-people-stock.jpg",
+    "scores": [
+        4,
+        5,
+        4,
+        5,
+        4,
+        5,
+        4,
+        5,
+        1,
+        1
+    ]
+},
+{
+    "name": "Lisa",
+    "photo": "https://i.kym-cdn.com/photos/images/original/000/954/163/99f.jpg",
+    "scores": [
+        5,
+        2,
+        1,
+        3,
+        3,
+        2,
+        1,
+        4,
+        5,
+        5
+    ]
+},
+{
+    "name": "Emily",
+    "photo": "https://images.pexels.com/photos/415829/pexels-photo-415829.jpeg?auto=compress&cs=tinysrgb&h=350",
+    "scores": [
+        4,
+        1,
+        4,
+        3,
+        1,
+        5,
+        2,
+        3,
+        4,
+        4
+    ]
+},
+{
+    "name": "Bill",
+    "photo": "https://media.istockphoto.com/photos/forty-something-man-picture-id502734204?k=6&m=502734204&s=612x612&w=0&h=7vBKfSGEyW0P2vgbYlYYuzSO_7Pj-BKt8-WCeN33GZ8=",
+    "scores": [
+        2,
+        2,
+        2,
+        3,
+        3,
+        1,
+        4,
+        5,
+        5,
+        3
+    ]
+},
+{
+    "name": "Joe",
+    "photo": "https://i.kym-cdn.com/photos/images/newsfeed/000/870/144/ba3.jpg",
+    "scores": [
+        5,
+        3,
+        4,
+        2,
+        1,
+        2,
+        3,
+        3,
+        2,
+        3
+    ]
+},
+{
+    "name": "Alice",
+    "photo": "http://www.lpcsoutheast.com/wp-content/uploads/2013/04/younger-business-woman.jpg",
+    "scores": [
+        2,
+        1,
+        1,
+        2,
+        3,
+        1,
+        1,
+        2,
+        4,
+        1
+    ]
+},
+{
+    "name": "Albert",
+    "photo": "https://pbs.twimg.com/media/CynmmdYWgAAjky1.jpg",
+    "scores": [
+        5,
+        5,
+        5,
+        5,
+        5,
+        5,
+        5,
+        5,
+        5,
+        5
+    ]
+}
+
+];
+
+module.exports = friends;
