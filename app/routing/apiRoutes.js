8f64ad8a0cb483fa476f282c28d63b5704702555
Doogle <fdunigan11@gmail.com>
2018-08-09 17:46:08 -0400

    Completed code for modal pop-up.

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,3 +1,5 @@
+var path = require('path');
+
 var friendsData = require("../data/friends");
 
 module.exports = function(app) {
@@ -9,14 +11,33 @@ module.exports = function(app) {
   app.post("/api/friends", function(req, res) {
 
     var newFriend = req.body;
+
+    var friendResponse = newFriend.scores;
+
+    var matchName = '';
+    var matchImage = '';
+    var difference = 10000;
+
+    for (var i = 0; i < friendsData.length; i++) {
+        var diff = 0;
+        for (var j = 0; j < friendResponse.length; j++) {
+            diff += Math.abs(friendsData[i].scores[j] - friendResponse[j]);
+        }
+
+        if (diff < difference) {
+            difference = diff;
+            matchName = friendsData[i].name;
+            matchImage = friendsData[i].photo;
+        }
+    }
   
     newFriend.routeName = newFriend.name.replace(/\s+/g, "").toLowerCase();
-  
+
     console.log(newFriend);
   
     friendsData.push(newFriend);
   
-    res.json(newFriend);
+    res.json({status: 'OK', matchName: matchName, matchImage: matchImage});
   });
 
 };
